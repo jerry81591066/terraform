44d8aadf7df7c9b19b822730960ae304278038fc
Jerry Hsu <contact@jerryh.su>
2021-04-16 15:04:05 +0800

    chore(struc): refactor repo structure

--- single-ec2/instance.tf
+++ single-ec2/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web-srv" {
   ami           = "ami-031b673f443c2172c" // Ubuntu Server 20.04 LTS
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.ssh_key.key_name
   vpc_security_group_ids = [
     aws_security_group.ssh.id,
